2d3a002a05cbd8fbbf44cba12d6a861104b127d3
arhturVicentine <arthurpsi36@gmail.com>
2022-03-03 13:55:06 -0300

    first commit

---
+++ exercicio008.py
@@ -0,0 +1,5 @@
+print('-='*20)
+print(f'{"CONVERSOR DE METROS" :^40}')
+print('-='*20)
+metragem =int(input('Digite um número em metros: '))
+print(f'o valor digitado em kilometros é {metragem / 1000}km ,\n em  centimetros {metragem * 100}cm \ne milimetros {metragem * 1000} mm')
